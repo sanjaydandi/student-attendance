0d2228a7d11a6176a3dd825a6a183b24060d9912
Unknown-O8 <160333974+Unknown-O8@users.noreply.github.com>
2025-03-27 09:55:36 +0530

    temp change

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ import threading
 
 # Disable TensorFlow oneDNN warnings
 os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 # Constants
 ATTENDANCE_FILE = "attendance.csv"
